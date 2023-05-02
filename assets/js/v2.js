a1a90e565ea4838d23db2d55be6cb5110a99c51f
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-05-03 00:41:51 +0430

    Fix `KeyError` bug

--- assets/js/v2.js
+++ assets/js/v2.js
@@ -1,4 +1,4 @@
-let address = { github: "github.com/python-bale-bot/python-bale-bot", documention: "docs.python-bale-bot.ir", bale: "bale.ai", dev_kian_ahmadian: "github.com/kian-ahmadian", dev_amin_shahrabi: "github.com/AminShahrabi" }
+let address = { github: "github.com/python-bale-bot/python-bale-bot", documention: "docs.python-bale-bot.ir", bale: "bale.ai", dev_kian_ahmadian: "github.com/kian-ahmadian", dev_amin_shahrabi: "github.com/AminShahrabi", dev_kasra_gamasaee: "github.com/kasragamasaee" }
 function openAddress(name){
     const key = name.replaceAll("-", "_");
     if (!address[key]) return;
