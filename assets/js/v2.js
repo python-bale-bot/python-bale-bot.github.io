143bc8fb93b345aecce579a0097a08ee89b35663
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-05-03 17:19:36 +0430

    Change size & Add more info button

--- assets/js/v2.js
+++ assets/js/v2.js
@@ -1,4 +1,4 @@
-let address = { github: "github.com/python-bale-bot/python-bale-bot", documention: "docs.python-bale-bot.ir", bale: "bale.ai", dev_kian_ahmadian: "github.com/kian-ahmadian", dev_amin_shahrabi: "github.com/AminShahrabi", dev_kasra_gamasaee: "github.com/kasragamasaee" }
+let address = { github: "github.com/python-bale-bot/python-bale-bot", github_examples: "github.com/python-bale-bot/python-bale-bot/tree/master/examples", documention: "docs.python-bale-bot.ir", bale: "bale.ai", dev_kian_ahmadian: "github.com/kian-ahmadian", dev_amin_shahrabi: "github.com/AminShahrabi", dev_kasra_gamasaee: "github.com/kasragamasaee", pypi: "pypi.org/project/python-bale-bot/" }
 function openAddress(name){
     const key = name.replaceAll("-", "_");
     if (!address[key]) return;
