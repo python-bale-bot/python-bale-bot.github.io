5d644a9909c7fc67b2de6e3a4700956b16d35a95
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-10-04 15:57:00 +0330

    Update changelog page
    * Add javascript file to convert english numbers to persian numbers
    * Update css file
    * Add new unstable version 2.4.8

---
+++ assets/js/changelog.js
@@ -0,0 +1,25 @@
+const words = [
+    ['1', '۱'],
+    ['2', '۲'],
+    ['3', '۳'],
+    ['4', '۴'],
+    ['5', '۵'],
+    ['6', '۶'],
+    ['7', '۷'],
+    ['8', '۸'],
+    ['9', '۹']
+]
+
+window.onload = () => {
+    const elements = document.getElementsByClassName("version")
+    for (let element of elements) element.innerHTML = replaceWords(element.innerHTML);
+    const a_elements = document.getElementsByTagName('a')
+    for (let element of a_elements) element.setAttribute('target', '_blank');
+}
+
+function replaceWords(element){
+    for (let index in words){
+        element = element.replaceAll(words[index][0], words[index][1])
+    }
+    return element
+}
