f0a0e34b8953e2149b6ea21dd0f838020ad817b0
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-10-21 21:26:48 +0330

    Improve Code Quality

--- assets/js/v2.js
+++ assets/js/v2.js
@@ -1,10 +1,3 @@
-const address = { github: "github.com/python-bale-bot/python-bale-bot", github_examples: "github.com/python-bale-bot/python-bale-bot/tree/master/examples", documention: "docs.python-bale-bot.ir", bale: "bale.ai", dev_kian_ahmadian: "github.com/kian-ahmadian", dev_amin_shahrabi: "github.com/AminShahrabi", dev_kasra_gamasaee: "github.com/kasragamasaee", pypi: "pypi.org/project/python-bale-bot/", bale_bot_developers: "dev.bale.ai", bale_api_problems: "github.com/python-bale-bot/python-bale-bot/discussions/24", changelog: "python-bale-bot.ir/changelog" }
-function openAddress(name){
-    const key = name.replaceAll("-", "_");
-    if (!address[key]) return;
-    open("https://" + address[key], "_blank");
-}
-
 const words = [
     ['1', '۱'],
     ['2', '۲'],
