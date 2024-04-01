cbc17066f6599fd1c22bfada2a634d540a77bcde
Kian Ahmadian <info@kian-ahmadian.ir>
2024-04-01 06:11:38 +0330

    Add new faq and update copybutton section

--- assets/js/copybutton.js
+++ assets/js/copybutton.js
@@ -1,12 +1,14 @@
-const copyInstallCodeTextButton = document.getElementById('copy-pip-command-button')
-copyInstallCodeTextButton.onclick = () => {
-    if (copyInstallCodeTextButton.disabled) return;
-    const code = copyInstallCodeTextButton.parentElement.innerText.replace(copyInstallCodeTextButton.innerText + '\n', "");
-    navigator.clipboard.writeText(code);
-    copyInstallCodeTextButton.textContent = 'کپی شد'
-    copyInstallCodeTextButton.disabled = true;
-    setTimeout(() => {
-        copyInstallCodeTextButton.textContent = 'کپی'
-        copyInstallCodeTextButton.disabled = false; 
-    }, 3000)
-}
+const copyInstallCodeTextButtons = document.getElementsByClassName('copy-pip-command-button')
+
+for (const copyInstallCodeTextButton of copyInstallCodeTextButtons)
+    copyInstallCodeTextButton.onclick = () => {
+        if (copyInstallCodeTextButton.disabled) return;
+        const code = copyInstallCodeTextButton.parentElement.innerText.replace(copyInstallCodeTextButton.innerText + '\n', "");
+        navigator.clipboard.writeText(code);
+        copyInstallCodeTextButton.textContent = 'کپی شد'
+        copyInstallCodeTextButton.disabled = true;
+        setTimeout(() => {
+            copyInstallCodeTextButton.textContent = 'کپی'
+            copyInstallCodeTextButton.disabled = false; 
+        }, 3000)
+    }
